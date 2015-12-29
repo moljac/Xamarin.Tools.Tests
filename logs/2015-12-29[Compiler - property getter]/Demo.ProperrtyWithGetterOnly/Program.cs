f138ab4c48996b29e91ed56de7c5bb45dbf1f671
moljac <mcvjetko@holisticware.net>
2015-12-29 20:24:07 +0100

    compiler error in VS2013 property with getter

---
+++ logs/2015-12-29[Compiler - property getter]/Demo.ProperrtyWithGetterOnly/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.ProperrtyWithGetterOnly
+{
+    class Demo
+    {
+        public string PropertWithGetterFail
+        {
+            get
+            {
+                return "";
+            }
+        }
+
+        public string PropertWithGetterOK01
+        {
+            /*
+                Visual Studio 2013:
+                'Demo.ProperrtyWithGetterOnly.Demo.PropertWithGetterOK01.get' 
+                must declare a body because it is not marked abstract or extern. 
+                Automatically implemented properties must define both get and set accessors.
+             
+                Visual Studio 2015:     OK
+              
+                Xamarin.Studio:         OK
+             
+            */
+            get;
+        }
+
+        public string PropertWithGetterOK02
+        {
+            get;
+            private set;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
