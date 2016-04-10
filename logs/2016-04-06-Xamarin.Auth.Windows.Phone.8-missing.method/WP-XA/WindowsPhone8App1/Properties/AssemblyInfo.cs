0c5a57c9cf9cea0fdc378d1b4219decf2e8cae70
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-04-10 14:22:38 +0200

    project folders renamed

--- logs/2016-04-06-Xamarin.Auth.Windows.Phone.8-missing.method/WP-XA/WindowsPhone8App1/Properties/AssemblyInfo.cs
+++ logs/2016-04-06-Xamarin.Auth.Windows.Phone.8-missing.method/WP-XA/WindowsPhone8App1/Properties/AssemblyInfo.cs
@@ -1,18 +1,27 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("App1")]
+[assembly: AssemblyTitle("WindowsPhone8App1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("App1")]
+[assembly: AssemblyProduct("WindowsPhone8App1")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("283e9b3b-40df-4563-ac1f-76a34358db76")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -21,9 +30,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
