8e470dd8e77b78efb0b638ea592ae118a23d9a03
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-09-20 23:15:56 +0200

    payloads error bait n switch

---
+++ logs/2017-09-20-project-references-and-nuget-bait-n-switch-payload/samples/AppUsingLibraryUtilityBaitAndSwitch.XamarinAndroid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AppUsingLibraryUtilityBaitAndSwitch.XamarinAndroid
+{
+    [Activity(Label = "AppUsingLibraryUtilityBaitAndSwitch.XamarinAndroid", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
